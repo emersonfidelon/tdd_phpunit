0a176dde7182efa95984981a12bea921fc75491c
Emerson Santana <emersonfidelon@gmail.com>
2020-12-27 12:22:48 -0300

    Usage Stab from PHPUnit

--- src/OrderBundle/Test/Service/BadWordsValidatorTest.php
+++ src/OrderBundle/Test/Service/BadWordsValidatorTest.php
@@ -2,6 +2,7 @@
 
 namespace OrderBundle\Test\Service;
 
+use OrderBundle\Repository\BadWordsRepository;
 use OrderBundle\Service\BadWordsValidator;
 use PHPUnit\Framework\TestCase;
 
@@ -9,12 +10,40 @@ class BadWordsValidatorTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider badWordsDataProvider
     */
-    public function hasBadWords()
+    public function hasBadWords($badWordsList, $text, $foundBadWords)
     {
-        $badWordsRepository = new BadWordsRepositoryStub();
+        $badWordsRepository = $this->createMock(BadWordsRepository::class);
+        $badWordsRepository->method('findAllAsArray')->willReturn($badWordsList);
         $badWordsValidator = new BadWordsValidator($badWordsRepository);
-        $hasBadWords = $badWordsValidator->hasBadWords('Seu restaurante fede');
-        $this->assertEquals(false, $hasBadWords);
+        $hasBadWords = $badWordsValidator->hasBadWords($text);
+        $this->assertEquals($foundBadWords, $hasBadWords);
+    }
+
+    public function badWordsDataProvider()
+    {
+        return [
+            'shouldFindWhenHasBadWords' => [
+                'badWordsList' => ['bobo','besta', 'fedido'],
+                'text' => 'Seu sapato é muito fedido',
+                'foundBadWords' => true,
+            ],
+            'shouldNotFindWhenNoHasBadWords' => [
+                'badWordsList' => ['bobo','besta', 'fedido'],
+                'text' => 'Seu sapato é bonito',
+                'foundBadWords' => false,
+            ],
+            'shouldNotFindWhenTextIsEmpty' => [
+                'badWordsList' => ['bobo','besta', 'fedido'],
+                'text' => '',
+                'foundBadWords' => false,
+            ],
+            'shouldNotFindWhenBadWordsListIsEmpty' => [
+                'badWordsList' => [],
+                'text' => 'Seu sapato é fedido',
+                'foundBadWords' => false,
+            ]
+        ];
     }
 }
